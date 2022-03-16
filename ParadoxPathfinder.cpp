a1e3dfdf749b9945818b45ec4d2850c0c58ad3b5
Seth Nielsen <seth.emanuel.nielsen@gmail.com>
2022-03-16 15:06:41 +0100

    take 3

--- ParadoxPathfinder.cpp
+++ ParadoxPathfinder.cpp
@@ -3,15 +3,16 @@
 
 #include <iostream>
 #include <iomanip>
+#include <chrono>
 #include <queue>
 #include <climits>
 #include <tuple>
+#include <stdlib.h>
 
 using namespace std;
 
-// Small typedefs for ease of reading
-typedef pair<int, int> ipair;
-typedef tuple<int, int, int> itriplet;
+// Small typedef for ease of reading
+typedef tuple<int, int, long long> itriplet;
 
 // Declare functions that will be used
 bool FindPath(pair<int, int> Start,
@@ -20,18 +21,17 @@ bool FindPath(pair<int, int> Start,
     pair<int, int> MapDimensions,
     vector<int>& OutPath);
 
-inline int CalcDistance(int c1, int c2, int width);
+inline int CalcDistance(long long c1, long long c2, int width);
 
 template<typename T>
 void PrintVectorToSquare(vector<T>& vec, int width);
-
 
 int main()
 {
     
     vector<bool> map = {
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1,
+        1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
         1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
         1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1,
         1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1,
@@ -43,25 +43,35 @@ int main()
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     };
+    pair<int, int> Start(5, 11);
+    pair<int, int> Target(5, 1);
+    pair<int, int> MapDims(11, 12);
+    
 
     /*
     vector<bool> map = { 
-        1, 1, 1, 1, 
-        0, 1, 0, 1, 
-        0, 1, 1, 1 };
-    */
+        1,0,0,0,0,0,0,1};
     
-    ipair Start(5, 11);
-    ipair Target(5, 1);
-    ipair MapDims(11, 12);
+    pair<int,int> Start(0, 0);
+    pair<int, int> Target(7, 0);
+    pair<int, int> MapDims(8, 1);
+    */
 
     vector<int> outPath;
-
+    
     //PrintVectorToSquare<bool>(map, MapDims.first);
-
+    auto start_time = std::chrono::high_resolution_clock::now();
     bool hej = FindPath(Start, Target, map, MapDims, outPath);
+
+    cout << "answer was " << hej << endl;
     for (int cur : outPath)
         cout << cur << endl;
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto time = end_time - start_time;
+
+    std::cout << "program took " <<
+        time / std::chrono::milliseconds(1) << "ms to run.\n";
 }
 
 bool FindPath(  pair<int, int> Start,
@@ -69,23 +79,32 @@ bool FindPath(  pair<int, int> Start,
                 const vector<bool>& Map,
                 pair<int, int> MapDimensions,
                 vector<int>& OutPath) {
-
-    // Setup memory notepads
-    vector<int> costF(Map.size(),INT_MAX);
-    vector<int> costG(Map.size(),INT_MAX);
-    vector<int> parent(Map.size(), -1);
-    vector<bool> visited(Map.size(), false);
     
     // translate input pos's to single-int coordinates
     int iStart  = Start.first   + Start.second* MapDimensions.first;
     int iTarget = Target.first  + Target.second* MapDimensions.first;
+    if (iStart == iTarget) return true;
+
+    /*
+    // Setup memory notepads
+    vector<int> costF(Map.size(), INT_MAX);
+    vector<int> costG(Map.size(), INT_MAX);
+    vector<int> parent(Map.size(), -1);
+    vector<bool> closed(Map.size(), false);
+    */
+    
+    // Setup memory notepads
+    int* costF = new int[Map.size()]; fill(costF, costF + Map.size(), INT_MAX);
+    int* costG = new int[Map.size()]; fill(costG, costG + Map.size(), INT_MAX);
+    int* parent = new int[Map.size()]; fill(parent, parent + Map.size(), -1);
+    bool* closed = new bool[Map.size()]; fill(closed, closed + Map.size(), 0);
+    
 
     // Setup p-queue comparer, tie-breaker as recently added.
     auto compare = [](const itriplet &lhs, const itriplet &rhs)
     {
-        return get<0>(lhs) > get<0>(rhs) || (get<0>(lhs) == get<0>(rhs) && get<1>(lhs) < get<1>(rhs));
+        return get<0>(lhs) > get<0>(rhs) || (get<0>(lhs) == get<0>(rhs) && get<1>(lhs) > get<1>(rhs));
     };
-    
     priority_queue<itriplet, vector<itriplet>, decltype(compare)> p_frontier(compare);
     
     // Setup frontier and add starting point
@@ -100,8 +119,12 @@ bool FindPath(  pair<int, int> Start,
 
     // Keep searching while valid frontier exists
     while (!p_frontier.empty()) {
-        int cursor = get<2>(p_frontier.top());
+        long long cursor = get<2>(p_frontier.top());
         p_frontier.pop();
+
+        //If already evaluated, skip
+        if (closed[cursor]) continue;
+        closed[cursor] = true;
 
         //If cursor is goal, back-trace path and return true
         if (cursor == iTarget) {
@@ -109,18 +132,9 @@ bool FindPath(  pair<int, int> Start,
                 OutPath.insert(OutPath.begin() + 0, cursor);
                 cursor = parent[cursor];
             }
-            // DEBUG only: Print memory-matrices contents
-            PrintVectorToSquare<int>(costG, MapDimensions.first);
-            PrintVectorToSquare<int>(costF, MapDimensions.first);
-            PrintVectorToSquare<bool>(visited, MapDimensions.first);
+            delete[] costF; delete[] costG; delete[] parent; delete[] closed;
             return true;
         }
-
-        //If already evaluated, skip
-        if (visited[cursor]) continue;
-
-        visited[cursor] = true;
-
 
         //Check for valid neighboor-directions
         neighValid[0] = cursor % MapDimensions.first != 0;
@@ -131,26 +145,22 @@ bool FindPath(  pair<int, int> Start,
         //Enqueue neighboors if qualified
         for (int i = 0; i < 4; i++) {
             if (!neighValid[i]) continue;
-            int newCur = cursor + neigh[i];
-            if (!visited[newCur] && Map[newCur] && costG[cursor] + 1 < costG[newCur]) {
+            long long newCur = cursor + neigh[i];
+            if (!closed[newCur] && Map[newCur] && costG[cursor] + 1 < costG[newCur]) {
                 costG[newCur] = costG[cursor] + 1;
-                costF[newCur] = costG[newCur] + CalcDistance(newCur, iTarget, MapDimensions.first);
+                int costH = CalcDistance(newCur, iTarget, MapDimensions.first);
+                costF[newCur] = costG[newCur] + costH;
                 parent[newCur] = cursor;
-                p_frontier.emplace(itriplet(costF[newCur], round++, newCur));
+                p_frontier.emplace(itriplet(costF[newCur], costH, newCur));
             }
         }
     }
-
-    // DEBUG only: Print memory-matrices contents
-    //PrintVectorToSquare<int>(costG, MapDimensions.first);
-    //PrintVectorToSquare<int>(costF, MapDimensions.first);
-    //PrintVectorToSquare<bool>(visited, MapDimensions.first);
-
     // If queue emptied not having reached Target, we return false
+    delete[] costF; delete[] costG; delete[] parent; delete[] closed;
     return false;
 }
 
-inline int CalcDistance(int c1, int c2, int width) {
+inline int CalcDistance(long long c1, long long c2, int width) {
     // Calculate the Manhattan-distance between two single-int coordinates in a grid.
     return abs(c1 / width - c2 / width) + abs(c1 % width - c2 % width);
 }
